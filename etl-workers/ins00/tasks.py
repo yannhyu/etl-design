e740aa096abfa2a6faeecacb5e3fce4d1b004693
Yann Yu <yann.yu@medlytix.com>
2017-02-01 12:24:33 -0500

    more logging

--- etl-workers/ins00/tasks.py
+++ etl-workers/ins00/tasks.py
@@ -9,6 +9,18 @@ from sqlalchemy import create_engine
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler('tasks.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
 
 @app.task(name='ins00.add')
 def add(x, y):
