4cd3e1afb26afd7c2bcf8cee47dfc847681981f7
Yann Yu <yann.yu@medlytix.com>
2017-02-02 11:41:56 -0500

    add flex find capability

--- etl-workers/ins00/tasks.py
+++ etl-workers/ins00/tasks.py
@@ -61,3 +61,38 @@ def read_db_data(lname_wanted='Aarick'):
     for ins in insurances:    
         results.append('{} {} {}<br>\n'.format(ins.data['fname'], ins.data['lname'], ins.data['ssn']))
     return ''.join(results)
+
+@app.task(name='ins00.flex_find_data')
+def flex_find_data(**kwargs):
+    CONN_STRING = 'postgresql://test_user:med@10.20.20.12:5432/etl'
+    Base = automap_base()
+
+    # engine, assume it has a table 'ins00' set up
+    engine = create_engine(CONN_STRING)
+
+    # reflect the tables
+    Base.prepare(engine, reflect=True)
+
+    # mapped classes are now created with names by default
+    # matching that of the table name.
+    Insurance = Base.classes.ins00
+    session = Session(engine)
+
+#    from sqlalchemy import text
+#    stmt = text("SELECT id, data "
+#                "FROM ins00 WHERE LOWER(data->>'lname')=LOWER(:lname)")
+#    stmt = stmt.columns(Insurance.id, Insurance.data)
+
+#    LNAME_WANTED = lname_wanted
+#    logger.info('looking up by last name: {}'.format(LNAME_WANTED))
+
+#    insurances = session.query(Insurance).\
+#                 from_statement(stmt).params(lname=LNAME_WANTED).all()
+#    results = []
+#    for ins in insurances:
+#        results.append('{} {} {}<br>\n'.format(ins.data['fname'], ins.data['lname'], ins.data['ssn']))
+#    return ''.join(results)
+    results = []
+    for key, value in kwargs.items():
+        results.append("{} = {}<br>\n".format(key, value))
+    return ''.join(results)
--- web-app/app.py
+++ web-app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask import url_for
 from worker import celery
 from celery.result import AsyncResult
@@ -20,6 +20,13 @@ def read_db_data(param):
     return "<a href='{url}'>check status of {id} </a>".format(id=task.id,
                 url=url_for('check_task',id=task.id,_external=True))
 
+@app.route('/flex_find_data/')
+def flex_find_data():
+    all_args = request.args.to_dict()
+    task = celery.send_task('ins00.flex_find_data', args=[], kwargs=jsonify(all_args))
+    return "<a href='{url}'>check status of {id} </a>".format(id=task.id,
+                url=url_for('check_task',id=task.id,_external=True))
+
 @app.route('/check/<string:id>')
 def check_task(id):
     res = celery.AsyncResult(id)
@@ -27,6 +34,8 @@ def check_task(id):
         return res.state
     else:
         return str(res.result)
+
+
 if __name__ == '__main__':
     app.run(debug=env.get('DEBUG',True),
             port=int(env.get('PORT',5000)),
