f4bafd21a068f3284ca7f67b9149aa6f49c10546
Yann Yu <yann.yu@medlytix.com>
2017-02-03 15:30:53 -0500

    confused or clear

--- etl-workers/ins00/tasks.py
+++ etl-workers/ins00/tasks.py
@@ -111,8 +111,11 @@ def flex_find_data(*args, **kwargs):
         fname = ins.data.get('eb_fn')
         append_ssn = ins.data.get('eb_ssn')
         if fname or append_ssn:
-            results.append('<h5 style="color:red;">')
-            results.append('(eBureau: fname:{}, append_ssn:{})'.format(fname, append_ssn))
+            results.append('<h5 style="color:red;">(eBureau: ')
+            if fname:
+                results.append('fname: {} '.format(fname))
+            if append_ssn:
+                results.append('append_ssn: {})'.format(append_ssn))
             results.append('</h5>')
         results.append('<br>\n<br>\n')
     return ''.join(results)
